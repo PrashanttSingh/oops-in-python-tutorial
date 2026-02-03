30ba78ed2aa57e9f474729f79af7b919a9ad5884
Its Prashant <Prashantsingh3993@gmail.com>
2026-02-03 22:23:22 +0530

    Added signup feature

--- oops_project.py
+++ oops_project.py
@@ -1,3 +1,5 @@
+import time
+
 class logogram:
   def __init__(self):
     self.username=''
@@ -12,15 +14,35 @@ class logogram:
                           3. press 3 to make or write your first post
                           4. press 4 to message a friend
                           5. press any other key to exit the app''')
+      time.sleep(1)
       if user_input =='1':
-        pass
+        print('Wait! Getting things ready for you')
+        time.sleep(2)
+        self.signup()
+      
       elif user_input =='2':
-          pass
+        print('Wait! Getting things ready for you')
+        time.sleep(2)
+        pass
       elif user_input =='3':
+          print('Wait! Getting things ready for you')
+          time.sleep(2)
           pass
       elif user_input =='4':
+          print('Wait! Getting things ready for you')
+          time.sleep
           pass
       else:
+          time.sleep(0.5)
+          print('Thankyou for using logogram,see you soon')
           exit()    
-          
-obj=logogram()          
+  def signup(self):
+    email=input('Enter your email')
+    time.sleep(0.5)     
+    pwd=input('Enter your Password')
+    self.username=email
+    self.password=pwd
+    self.menu
+    
+    
+obj=logogram()        
