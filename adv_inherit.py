1299dace620f3cb98b223d26b526e7988ff3b741
prashant singh <prashantsingh3993@gmail.com>
2026-02-06 23:17:53 +0530

    multi-level inheritence

--- adv_inherit.py
+++ adv_inherit.py
@@ -1,17 +1,38 @@
-#single or basic inheritence
-class parent:
-  def __init__(self,name):
-    parent.self=name
+# class parent:
+#  def __init__(self,name):
+#     parent.self=name
 
-  def goods(self):
-    print(f'{parent.self} has good handwritting...')  
+#  def goods(self):
+#     print(f'{parent.self} has good handwritting...')  
+
+# class child(parent):
+#   def goods(self):
+#     print(f'yes  my dad says true i({parent.self}) has good handwritting...')
+
+# # parent=parent('alice')    
+# # parent.goods()
+# child=child('alice')
+# child.goods()
+#----------------------------------------------------------------------------
+
+#multi-level inheritence
+class grandparent:
+  def __init__(self,color):
+    self.color=color
+
+  def loves(self):
+    print(f'grandparents love {self.color} color')
+
+class parent(grandparent):
+  def does(self):
+    print(f'parents love just like grandparents {self.color} color ')
 
 class child(parent):
-  def goods(self):
-    print(f'yes  my dad says true i({parent.self}) has good handwritting...')
+  def do(self):
+    print(f'child loves same colour as the parents love the {self.color} color')
 
-# parent=parent('alice')    
-# parent.goods()
-child=child('alice')
-child.goods()
-#----------------------------------------------------------------------------
+child=child('blue')
+child.loves() 
+child.do() 
+child.does()
+#-----------------------------------------------------------------------------------
